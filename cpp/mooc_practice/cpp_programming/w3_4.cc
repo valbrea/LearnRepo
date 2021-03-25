c95ed88906139a013d8eda85a1e6f4a4f3a6646f
valbrea <val.brea@qq.com>
2021-03-25 16:03:03 +0800

    2021-03-25

--- cpp/mooc_practice/cpp_programming/w3_4.cc
+++ cpp/mooc_practice/cpp_programming/w3_4.cc
@@ -1,5 +1,6 @@
-/*
-描述
+/* 编程题＃4：魔兽世界之一：备战
+
+描述:
 魔兽世界的西面是红魔军的司令部，东面是蓝魔军的司令部。两个司令部之间是依次排列的若干城市。
 红司令部，City 1，City 2，……，City n，蓝司令部
 两军的司令部都会制造武士。武士一共有 dragon 、ninja、iceman、lion、wolf
@@ -62,53 +63,94 @@ Case:1
 #include <iostream>
 #include <string>
 using namespace std;
-class Samurai {
+class Camp {
  public:
-  int number = 1,  // 记录编号
-      qty[5],      // 记录每种武士的数量
-      s_command;   // 记录司令部总共多少生命
+  string camp_name_;  // 司令部名字
+  int number_,        // 记录生产编号
+      qty_[5],        // 记录每种武士的数量
+      camp_str_;      // 记录司令部总共多少生命
 
-  static int strength[5],  // 记录每种武士的生命
-      attack[5],           // 记录每种武士的攻击力
-      minstr;              // 记录最少需要消耗生命的量
-  void BuildSamurai() {
-    for (int time(0);; ++time) {
-      for (int i(0); i < 5; ++i) {
-        if (s_command <) }
-    }
+  string type_in_order_[5];  // 两个阵营按顺序记录的出兵名称
+  int str_in_order_[5],      // 两个阵营按顺序记录的武士的生命值
+      atk_in_order_[5],      // 两个阵营按顺序记录的武士的攻击力
+      index_;                // 当前的出兵位置
+
+  static int min_str_,  // 记录最少需要消耗生命的量
+      time;             // 武士出兵时间
+  Camp(const string camp_name, const int &camp_str, const int *str_per,
+       const string *type, const int *seq);  // 构造函数声明
+  int BuildWarriors();                       // 给当前阵营制造武士
+};
+// *************构造函数定义**********
+Camp::Camp(const string camp_name, const int &camp_str, const int *str_per,
+           const string *type, const int *seq)
+    : camp_name_(camp_name),
+      number_(1),  // 编号从1开始
+      qty_{0},
+      camp_str_(camp_str),
+      type_in_order_{0},
+      str_in_order_{0},
+      atk_in_order_{0},
+      index_(0) {
+  time = 0;             // 两军共用一个时间
+  min_str_ = camp_str;  // 最小值先设定一个较高的数
+  for (int i(0); i < 5; ++i) {
+    type_in_order_[seq[i]] = type[i];
+    str_in_order_[seq[i]] = str_per[i];
+    if (str_in_order_[seq[i]] < min_str_) min_str_ = str_in_order_[seq[i]];
   }
-  bool CheckLeftLife() { if () }
-} red, blue;
-
+}
+// **************主函数***************
 int main() {
-  int n(0), time(0);
-  Samurai red, blue;
-  const int red_seq[5] = {2, 3, 4, 1, 0}, blue_seq[5] = {3, 0, 1, 2, 4};
-  static Samurai.type[5] = {"dragon", "ninja", "iceman", "lion", "wolf"};
+  int n(0), camp_str(0), str_per[5]{0};
+  const string type[5]{"dragon", "ninja", "iceman", "lion", "wolf"};  //武士类型
+  const int red_seq[5]{2, 3, 4, 1, 0},  // 红方武士制造序列
+      blue_seq[5]{3, 0, 1, 2, 4};       // 蓝方武士制造序列
+
   cin >> n;  // 输入测试组数
-  do {
+  do {       // 本组测试完之前 do-while循环
     cout << "Case:" << n << endl;
     // 输入各组数据
-    cin >> Samurai::s_command;
+    cin >> camp_str;
     for (int i(0); i < 5; ++i) {
-      cin >> Samurai::strength[i];
-      if (Samurai::strength[i] < Samurai::minstr) {
-        Samurai::minstr = Samurai::strength[i];
-      }
+      cin >> str_per[i];
     }
+    // 初始化两个阵营
+    Camp red("red", camp_str, str_per, type, red_seq);
+    Camp blue("blue", camp_str, str_per, type, blue_seq);
 
-    // 制造武士
-    red.BuildSamurai();
-    blue.buildSamurai();
-
-    cout << setw(3) << setfill('0') << time << " red " << red.type << ' '
-         << red.number << " born with strength " << red.strength << ','
-         << red.qty << ' ' << red.type << " in red headquarter" << endl;
-    cout << setw(3) << setfill('0') << time << " blue " << blue.type << ' '
-         << blue.number << " born with strength " << blue.strength << ','
-         << blue.qty << ' ' << blue.type << " in blue headquarter" << endl;
+    // 分别制造武士
+    int r_end(0), b_end(0);
+    while (r_end != 0 && b_end != 0) {
+      ++Camp::time;
+      r_end = red.BuildWarriors();
+      b_end = blue.BuildWarriors();
+    }
 
   } while (--n > 0);
 
   return 0;
+}
+//
+int Camp::BuildWarriors() {
+  if (camp_str_ < min_str_) {  // 2) 司令部停止制造武士
+    cout << setw(3) << setfill('0') << time << ' ' << camp_name_
+         << "headquarter stops making warriors" << endl;
+    return 2;  // 返回2，表示当前这个阵营出兵完毕
+  } else {     // 1) 武士降生
+    if (camp_str_ < str_in_order_[index_]) { // 如果现有阵营生命值小于当前生产序列所需生命值
+      if (index_ != 4) // 如果不是最后一个武士，就试图制造下一个
+        index_++;
+      else
+        index_ = 0; // 如果时最后一个武士，就回到第一个武士
+    } else {
+      number_++;
+      camp_str_ -= str_in_order_[index_];
+      cout << setw(3) << setfill('0') << time << ' ' << camp_name_ << ' '
+           << type_in_order_ << ' ' << number_ << " born with strength "
+           << str_in_order_ << ',' << qty_ << ' ' << type_in_order_ << " in "
+           << camp_name_ << " headquarter" << endl;
+      return 0; // 返回0 继续循环
+    }
+  }
 }
