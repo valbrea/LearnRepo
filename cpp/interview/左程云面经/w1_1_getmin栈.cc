36635a07cf625f2c4436f9fabc17fddf64a0e00e
valbrea <val.brea@qq.com>
2021-05-18 22:01:55 +0800

    2021-05-18

--- cpp/interview/左程云面经/w1_1_getmin栈.cc
+++ cpp/interview/左程云面经/w1_1_getmin栈.cc
@@ -1,3 +1,41 @@
+#include <iostream>
+#include <stack>
+using namespace std;
 class GetMinStack {
-  
+private:
+  stack<int> data_;
+  stack<int> min_;
+
+public:
+  void Push(int new_num) {
+    if (min_.empty())
+      min_.push(new_num);
+    else if (new_num <= GetMin())
+      min_.push(new_num);
+    else
+      ;
+    data_.push(new_num);
+  }
+  int Pop() {
+    if (data_.empty()) {
+      cerr << "Your stack is empty." << endl;
+      return -1;
+    }
+    int value = data_.top();
+    data_.pop();
+    if (value == GetMin())
+      min_.pop();
+    return value;
+  }
+  int GetMin() {
+    if (min_.empty()) {
+      cerr << "Your stack is empty." << endl;
+      return -1;
+    }
+    return min_.top();
+  }
+};
+class GetMinStack2 {
+// 思路： min_中同步压入new_num和min_.top()两者中较小的元素；
+// 一直弹出min_.top()
 };
---
+++ cpp/mooc_practice/data_structure/w8_1.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+  system("pause");
+  return 0;
+}
