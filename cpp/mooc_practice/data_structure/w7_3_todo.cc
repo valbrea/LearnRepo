a51af23bcc516662f8b8eaff6b95c3da0c8a066d
valbrea <val.brea@qq.com>
2021-05-24 18:15:48 +0800

    2021-05-24

--- cpp/mooc_practice/data_structure/w7_3_todo.cc
+++ cpp/mooc_practice/data_structure/w7_3_todo.cc
@@ -50,3 +50,48 @@
 样例输出
 3
 */
+#define LOCAL // 本地调试宏定义，提交代码时注释掉此行
+#define STL   // STL库，不用时注释掉此行
+#define INF 0x3f3f3f3f
+#define INF_LL 0x3f3f3f3f3f3f3f3f
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#ifdef STL
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#endif
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+#ifdef LOCAL
+  freopen(".debug/data.in", "r", stdin);
+#endif
+
+  int n, k;  
+  cin >> n >> k;
+  int d, x ,y;
+  while (k--) {
+    // d:说法的种类， 1，x和y是同类，否则是x吃y
+    cin >> d >> x >> y;
+
+  }
+
+#ifdef LOCAL
+  cout << endl 
+       << "Runtime: " << 1000.0 * (double)clock() / CLOCKS_PER_SEC << "ms\n";
+#endif
+
+  return 0;
+}
